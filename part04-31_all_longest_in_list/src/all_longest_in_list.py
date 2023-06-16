ed677eca13244a1971b11333b704b4279110f0ad
aliimI <aliim12025@gmail.com>
2023-06-16 16:08:33 +0600

    part 4-4-definite iteration done

--- part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -3,16 +3,17 @@ def all_the_longest(my_list: list):
     new_list =[]
     first = 0
     for i in my_list:
-        if len(i) > first:
+        if len(i) >= first:
             first = len(i)
 
     for i in my_list:
         if len(i) == first:
             new_list.append(i)
+
             
     return new_list
 
 if __name__ == "__main__":
-    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    my_list = ["first", "second", "fourth", "eleventh"]
     result = all_the_longest(my_list)
-    print(result) # ['dorothy', 'richard']
+    print(result) 
