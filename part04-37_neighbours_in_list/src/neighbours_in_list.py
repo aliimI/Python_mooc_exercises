99ed74f142d2a28e14beb290f55cbd0ac8930bc5
aliimI <aliim12025@gmail.com>
2023-06-20 11:09:24 +0600

    series of longest fone

--- part04-37_neighbours_in_list/src/neighbours_in_list.py
+++ part04-37_neighbours_in_list/src/neighbours_in_list.py
@@ -1,1 +1,19 @@
-# Write your solution here
+# Write your solution here  
+def longest_series_of_neighbours(my_list: list):
+    current_length = 1
+    longest_length = 1
+
+    for i in range(1, len(my_list)):
+        if abs(my_list[i-1]-my_list[i]) == 1:
+            current_length += 1
+        else:
+            current_length = 1
+
+        if current_length > longest_length:
+            longest_length = current_length
+    return longest_length
+    
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 5, 6, 9, 10]
+    print(longest_series_of_neighbours(my_list))
